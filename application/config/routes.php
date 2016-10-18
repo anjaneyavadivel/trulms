ffa3cbb479ce02c277f9657fbba00103eb378db7
USER <USER@KUMAR-PC>
2016-10-18 09:37:41 +0530

    karthik on 17-10-2016-3  ,routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,10 +49,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['dashboard'] = 'welcome/dashboard';
-$route['login'] = 'welcome/login';
-$route['logout'] = 'welcome/logout';
-$route['forgot-password'] = 'welcome/forgot_password';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+
+$route['default_controller'] 			= 		'welcome';
+$route['dashboard'] 					=		'welcome/dashboard';
+$route['login'] 						= 		'welcome/login';
+$route['logout']						=		'welcome/logout';
+$route['forgot-password'] 				=		'welcome/forgot_password';
+
+
+
+/*******************************************manage Controller*****************************/
+
+
+
+$route['department'] 					=		'manage/department';
+$route['add_department'] 				=		'manage/add_department';
+$route['edit_department'] 				=		'manage/edit_department';
+
+$route['designation'] 					=		'manage/designation';
+$route['add_designation'] 				=		'manage/add_designation';
+$route['edit_designation'] 				=		'manage/edit_designation';
+
+$route['role'] 							=		'manage/role';
+$route['add_role'] 						=		'manage/add_role';
+$route['edit_role'] 					=		'manage/edit_role';
+
+$route['payment_mode'] 					=		'manage/payment_mode';
+$route['add_payment_mode'] 				=		'manage/add_payment_mode';
+$route['edit_payment_mode'] 			=		'manage/edit_payment_mode';
+
+$route['payment_status'] 				=		'manage/payment_status';
+$route['add_payment_status'] 			=		'manage/add_payment_status';
+$route['edit_payment_status'] 			=		'manage/edit_payment_status';
+
+$route['employee_types'] 				=		'manage/employee_types';
+$route['add_employee_types'] 			=		'manage/add_employee_types';
+$route['edit_employee_types'] 			=		'manage/edit_employee_types';
+
+
